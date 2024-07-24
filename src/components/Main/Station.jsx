9b73158297e0de86d27759805f575ae561b08cb9
Minn-Choi <min031106@dgu.ac.kr>
2024-07-24 17:37:36 +0900

    feat : Main, Mypage, Train

---
+++ src/components/Main/Station.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./style";
+
+function TopStation() {
+  return (
+    <S.Frame>
+      <S.Bar />
+        <S.CenterBar>
+        </S.CenterBar>
+      <S.Bar />
+    </S.Frame>
+  );
+}
+
+export default TopStation;
---
+++ src/pages/Test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from '../components/Mypage/Search';
+import Station from '../components/Main/Station';
+import Menu from '../components/Main/Menu';
+import '../App.css';
+
+const Test = () => {
+  return (
+    <div>
+      <Search />
+      <Menu />
+      <Station />
+    </div>
+  );
+}
+
+export default Test;
