9df53670d8736cdd9c0b1e2a307be1611febf700
박성재 <sem201@naver.com>
2024-07-24 17:37:36 +0900

    feat : mainpage 생성

---
+++ src/components/intro2/Head.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./styled";
+
+
+function Head(){
+    return(
+        <S.HeadContainer>
+            <S.Title>BOILED<br/>TRAIN</S.Title>
+            <S.Content>삶은기차소개글두줄정도?<br/>
+    리네삶은지하철에서시작해지하철에서끝납니다.</S.Content>
+        </S.HeadContainer>
+    )
+}
+
+export default Head;
