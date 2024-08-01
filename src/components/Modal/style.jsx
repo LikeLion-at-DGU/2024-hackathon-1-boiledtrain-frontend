49766fb5cb3f38f49c3a7fb2b7d313474a81b6fc
Minn-Choi <min031106@dgu.ac.kr>
2024-08-01 14:03:56 +0900

    test

---
+++ src/components/Modal/style.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const ModalTotal = styled.div`
+  width: 430px;
+  height: 685px;
+  flex-shrink: 0;
+  border-radius: 60px 60px 0px 0px;
+  border: 1px solid #D9D9D9;
+  background: #FFF;
+  box-shadow: 0px 3px 21.5px 1px rgba(0, 0, 0, 0.25);
+`;
+
+export const Icon = styled.img`
+  width: 155px;
+  height: 37px;
+`;
+
+export const Head = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+  display: flex;
+  width: 430px;
+  height: 71px;
+  padding: 0px 137px 0px 138px;
+`;
+
+export const Body = styled.div`
+display:flex;
+flex-direction:column;
+`;
+
+export const Box = styled.div`
+display:flex;
+flex-direction:row;
+`;
+
+export const Btn = styled.div`
+display:flex;
+flex-direction:row;
+`;
+
+export const ClosedBtn = styled.button`
+width: 103px;
+height: 39px;
+flex-shrink: 0;
+border-radius: 20px;
+background: #00ABFC;
+`;
+
+export const PushBtn = styled.button`
+width: 188px;
+height: 39px;
+flex-shrink: 0;
+border-radius: 20px;
+background: #00ABFC;
+`;
