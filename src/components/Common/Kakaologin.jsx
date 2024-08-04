756d169f7ceeaf7fd2e492f35b22c50fa823b783
박성재 <sem201@naver.com>
2024-08-04 19:21:13 +0900

    hotfix : 카카오 로그인

--- src/components/Common/Kakaologin.jsx
+++ src/components/Common/Kakaologin.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const Kakaologin = () => {
     const redirectToKakaoLogin = async () => {
         // 서버 배포용
-        // const kakaoAuthUrl = 'http://3.36.243.22/api/accounts/kakao/login/';
+        const kakaoAuthUrl = 'http://3.36.243.22/api/accounts/kakao/login/';
         // 로컬 테스트용
-        const kakaoAuthUrl = 'http://3.36.243.22/api/accounts/front_kakao/login/';
+        // const kakaoAuthUrl = 'http://3.36.243.22/api/accounts/front_kakao/login/';
         window.location.href = kakaoAuthUrl;
     };
 
