3053764f4f3e26f33b3e67ac6cd6b070b1458aa3
Minn-Choi <min031106@dgu.ac.kr>
2024-08-06 12:31:47 +0900

    최종 수정

--- src/pages/DiaryDetail.jsx
+++ src/pages/DiaryDetail.jsx
@@ -6,6 +6,7 @@ import BottomBar from "../components/Common/BottomBar";
 import TopBarDiary from "../components/Common/TopBarDiary";
 import { getToken } from '../utils/auth';
 import Photo from '../assets/images/baseimage.png';
+import Point from '../assets/images/pointer.png';
 
 const DetailContainer = styled.div`
   width: 430px;
@@ -77,11 +78,11 @@ const DetailContent = styled.div`
 
 const DetailMood = styled.div`
   padding: 10px;
-  color: #000;
+  color: #00ABFC;
   text-align: center;
   font-family: 'Pretendard';
   font-size: 16px;
-  font-weight: 400;
+  font-weight: 600;
 `;
 
 const DiaryDetailLine = styled.div`
@@ -119,6 +120,12 @@ const BottomStyle = styled.div`
   width: 430px;
   height: 77px;
   background: #00ABFC;
+`;
+
+const DetailPhoto = styled.img`
+width:12px;
+padding-right:10px;
+padding-top:3px;
 `;
 
 const Detail = () => {
@@ -173,10 +180,10 @@ const Detail = () => {
       <DetailTitle>{data?.title}</DetailTitle>
       <DetailDate>{formatDate(data?.created_at)}</DetailDate>
       <DiaryDetailLine2 />
-      <DetailMood># {courseData?.subway_station || ''}역 코스</DetailMood>
-      <DetailURL onClick={handleCourseDetailClick} style={{ cursor: 'pointer' }}>
+      <DetailMood> <DetailPhoto src={Point}/>{courseData?.subway_station || ''}역 코스</DetailMood>
+      {/* <DetailURL onClick={handleCourseDetailClick} style={{ cursor: 'pointer' }}>
         코스 자세히 보기<UnderBar />
-      </DetailURL>
+      </DetailURL> */}
       <DetailThumbnail src={data?.image || Photo} alt="다이어리 이미지" />
       <DetailMood>{data?.mood}</DetailMood>
       <DetailContent>{data?.content}</DetailContent>
